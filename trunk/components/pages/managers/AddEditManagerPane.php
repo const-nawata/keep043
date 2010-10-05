8735788d689ddaaebe39e75f5cdfba536a262b31
nawataster@gmail.com <nawataster@gmail.com@abe4ae81-c1bb-c21b-33b0-b3c03f6747f7>
2010-10-05 18:48:37 +0000

    Initial copying.

---
+++ trunk/components/pages/managers/AddEditManagerPane.php
@@ -0,0 +1,134 @@
+<?php
+class AddEditManagerPane extends PAddEditPane{
+
+/**
+ * Constructor
+ * @access	public
+ * @param	object $Owner - object instanse to which object of this class belongs.
+ * @return	void
+ */
+    public function __construct( $Owner ){
+		parent::__construct( $Owner );
+
+		$this->mTitle		= _USER_PROFILE;
+
+    	$this->mName	= 'AddEditManagerPaneN';
+
+    	$this->mTarCss	= 'edit_pane_txt_ar';
+    	$this->mInpCss	= 'edit_pane_input';
+    	$this->mSelCss	= 'edit_pane_sel';
+
+    	$this->mWidth	= 455;
+    	$this->mHeigth	= 390;
+
+    	$this->mBrdClr	= _PANE_BORDER_COLOR;
+    	$this->mBkgClr	= _GEN_BKGRND_COLOR;
+    	$this->mInitFocus	= 'login';
+
+    }
+//--------------------------------------------------------------------------------------------------
+
+    public function initHtmlView(){
+    	$db_obj	= new Dbl( $this );
+    	$user_info	= $db_obj->getUserInfoById( $this->mRecId, $this->mOwner->getTargetDbTable() );
+
+    	$tanindex	= 1;
+    	$lines	= &$this->mLines;
+    	$lines	.= $this->getInputLineContent( 'login', 'text', _LOGIN, $user_info[ 'login' ], $tanindex, self::_onchange ); $tanindex++;
+    	$lines	.= $this->getInputLineContent( 'email', 'text', _EMAIL_ADDR, $user_info[ 'email' ], $tanindex, self::_onchange ); $tanindex++;
+    	$lines	.= $this->getInputLineContent( 'firstname', 'text', _USER_NAME, $user_info[ 'firstname' ], $tanindex, self::_onchange ); $tanindex++;
+    	$lines	.= $this->getInputLineContent( 'surname', 'text', _USER_SURNAME, $user_info[ 'surname' ], $tanindex, self::_onchange ); $tanindex++;
+
+    	$onchange	=
+    		self::_onchange.
+    		"xajax_corePpskHandler( \"".$this->getHandleResourceString( 'onChangeCountry', get_class( $this ) )."\", {\"country_id\":this.value,\"inst\":document.getElementById( \"inst\" ).value } );";
+
+    	$this->mOptions	= $db_obj->getCountriesList();
+    	$lines	.= $this->getSelBoxLineContent( 'country_id', _COUNTRY, $user_info[ 'country_id' ], $tanindex, $onchange ); $tanindex++;
+
+    	$country_id = ( !$this->mRecId ) ? $this->mOptions[ 0 ][ 'id' ] : $user_info[ 'country_id' ];
+    	$this->mOptions	= $db_obj->getCitiesList( $country_id );
+    	$lines	.= $this->getSelBoxLineContent( 'city_id', _CITY, $user_info[ 'city_id' ], $tanindex, self::_onchange ); $tanindex++;
+
+    	$lines	.= $this->getTextareaLineContent( 'info', _INFO, $user_info[ 'info' ], $tanindex, self::_onchange ); $tanindex++;
+
+    	$lines	.= $this->getInputLineContent( 'pass1', 'password', _NEW_PASSWORD, $user_info[ 'password' ], $tanindex, self::_onchange ); $tanindex++;
+    	$lines	.= $this->getInputLineContent( 'pass2', 'password', _CONF_PASSWORD, $user_info[ 'password' ], $tanindex, self::_onchange ); $tanindex++;
+    	parent::initHtmlView();
+    }
+//--------------------------------------------------------------------------------------------------
+
+    protected function isValidData( &$formValues ){
+	    if( _EMPTY == $formValues[ 'pass1' ] ){
+	    	$message	= sprintf( _MESSAGE_EMPTY_NAME, "`"._NEW_PASSWORD."`" );
+			$formValues	= array( 'focus_id' => 'pass1', 'description' => $message,  'is_valid' => false );
+			return;
+	    }
+	    if( _EMPTY == $formValues[ 'pass2' ] ){
+	    	$message	= sprintf( _MESSAGE_EMPTY_NAME, "`"._CONF_PASSWORD."`" );
+	    	$formValues	= array( 'focus_id' => 'pass2', 'description' => $message,  'is_valid' => false );
+	    	return;
+	    }
+
+	    if( $formValues[ 'pass1' ] != $formValues[ 'pass2' ] ){
+	    	$formValues	= array( 'focus_id' => 'pass1', 'description' => _MESSAGE_PASSWORDS_MISMATCH, 'is_valid' => false );
+	    	return;
+	    }
+
+	    $formValues[ 'is_valid' ]	= true;
+
+    }
+//--------------------------------------------------------------------------------------------------
+
+    protected function prepareData( &$formValues ){
+    	$this->mSaveData	= array(
+    		array( 'id',		$formValues[ 'id' ],		NULL ),
+    		array( 'firstname',	$formValues[ 'firstname' ],	'firstname' ),
+    		array( 'surname',	$formValues[ 'surname' ],	'surname' ),
+    		array( 'city_id',	$formValues[ 'city_id' ],	'city_id' ),
+    		array( 'level',		_LEVEL_MANAGER,				NULL ),
+    		array( 'info',		$formValues[ 'info' ],		'info' ),
+    		array( 'login',		$formValues[ 'login' ],		'login', _LOGIN ),
+    		array( 'email',		$formValues[ 'email' ],		'email' ),
+    		array( 'password',	$formValues[ 'pass1' ],		'pass1' )
+    	);
+    	parent::prepareData( $formValues );
+    }
+//--------------------------------------------------------------------------------------------------
+
+    public function onChangeCountry( &$objResponse, $ownerValues ){
+    	$this->getSessionParams( $ownerValues );
+    	$class		= $ownerValues[ 'class' ];
+    	$tabl_obj	= new $class( NULL, true, false );
+
+		$auth_obj = new Authentication();
+		if( $auth_obj->isGrantAccess( $tabl_obj->getAccess() ) ){
+			$db_obj	= new Dbl( $this );
+			$this->mOptions	= $db_obj->getCitiesList( $ownerValues[ 'country_id' ] );
+			$objResponse->addAssign( "city_id_cnt_td", 'innerHTML', $this->getSelBoxContent( 'city_id', 0, 4, self::_onchange ) );
+		}else{
+			$objResponse = $this->doAccessDenied();
+		}
+    }
+//--------------------------------------------------------------------------------------------------
+
+    public function saveInfo( &$objResponse, $formValues ){
+    	$result = parent::saveInfo( $objResponse, $formValues );
+
+    	if( !$result[ 'is_error' ] && $formValues[ 'action_type' ] == PAddEditPane::_add ){
+			$db_obj	= new Dbl( $this );
+	    	$result	= $db_obj->createDbViewsForNewManager( $formValues[ 'id' ] );
+	    	if( $result[ 'is_error' ] ){
+				$this->showAlertHandler( $objResponse, array( 'message' => $result[ 'description' ], 'focus' => $result[ 'focus_id' ] ) );
+	    	}
+    	}
+    }
+//--------------------------------------------------------------------------------------------------
+
+    public function __destruct(){
+    	parent::__destruct();
+    }
+//--------------------------------------------------------------------------------------------------
+
+}//	Class end
+?>
---
+++ trunk/components/pages/settings/tables/UnitsTable.php
@@ -0,0 +1,66 @@
+<?php
+require_once( $gl_pagesPath."settings/panes/AddEditUnitPane.php" );
+class UnitsTable extends PTable{
+
+	public function __construct( $Owner = NULL, $isHndl = false, $isInitView = true ){
+
+		//		$auth_obj = new Authentication();
+		//		if( $auth_obj->isGrantAccess( $this->mLevels ) ){
+
+		$this->setProperties();
+		parent::__construct( $Owner );
+		if( $isInitView ){ $this->initHtmlView( $isHndl ); }
+
+		//		}else{
+		//			global $gl_Path;
+		//			header('Location: access.php');
+		//		}
+	}
+	//--------------------------------------------------------------------------------------------------
+
+	/**
+	 * sets properties of table in constructor
+	 * @param	string $instName - uniq name for object instance.
+	 * @return void
+	 */
+	private function setProperties(){
+		$this->mLevels	= array( _LEVEL_ADMIN );
+		$this->mSourceDbTable	= 'units';
+		$this->mSelectorColor	= '#EDD3EA';
+		$this->mPgLen			= 16;
+		$this->mMaxGrPg			= 5;
+		$this->mUpperLine		= "<tr><td colspan='2' class='unitsTblUpperSellTtlTd'>"._PNAME1."</td></tr>";
+
+		$this->mIsFixHeight		= true;
+
+		$this->mColumns	= array(
+		array(
+	    		'field'		=> 'full_name',
+	    		'name'		=> _PFULL1,
+    			'ttl_css'	=> 'unitsTblFullTtlTd',
+    			'sll_css'	=> 'unitsTblFullClTd',
+    			'bg_clr'	=> '#FFF7E2',
+	    		'is_sort'	=> true
+		),
+		array(
+	    		'field'		=> 'brief_name',
+	    		'name'		=> _PBRIEF1,
+    			'ttl_css'	=> 'unitsTblBreifTtlTd',
+    			'sll_css'	=> 'unitsTblBreifClTd',
+    			'bg_clr'	=> '#FFF7E2',
+	    		'is_sort'	=> true
+		)
+		);
+
+		$this->setSearchFields( array( 'full_name', 'brief_name' ) );
+		$this->mPaneClassName	= 'AddEditUnitPane';
+	}
+	//--------------------------------------------------------------------------------------------------
+
+	public function __destruct(){
+		parent::__destruct();
+	}
+	//--------------------------------------------------------------------------------------------------
+
+}//	Class end
+?>
