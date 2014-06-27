4f95501720437c0c1528fc14f19e6dc1e1f65fc7
nawataster@gmail.com <nawataster@gmail.com@abe4ae81-c1bb-c21b-33b0-b3c03f6747f7>
2014-06-27 06:52:26 +0000

    Some changes.

--- ig/application/controllers/blog.php
+++ ig/application/controllers/blog.php
@@ -1,13 +1,14 @@
 <?php
 class Blog extends CI_Controller{
 
-
 	public function index(){
-
 		echo "Hello Dolly!!!";
-
-
 	}
 //------------------------------------------------------------------------------
 
-}
+	public function goods( $item=NULL, $id=NULL ){
+		echo "id: $id";
+	}
+//------------------------------------------------------------------------------
+
+}//	Class end
---
+++ ig/application/controllers/main.php
@@ -0,0 +1,15 @@
+<?php
+class Main extends CI_Controller{
+
+	public function __construct(){
+		parent::__construct();
+		// Your own constructor code
+	}
+//------------------------------------------------------------------------------
+
+	public function index(){
+		echo 'Main Url (index)';
+	}
+//------------------------------------------------------------------------------
+
+}//	Class end
