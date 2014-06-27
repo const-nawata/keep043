330691598581af96e0936a0dff327ec8de5ab410
nawataster@gmail.com <nawataster@gmail.com@abe4ae81-c1bb-c21b-33b0-b3c03f6747f7>
2014-06-27 09:02:59 +0000

    items

--- trunk/ig/application/views/main_page.php
+++ trunk/ig/application/views/main_page.php
@@ -2,11 +2,11 @@
 <html  xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Test Blog</title>
+	<title><?php echo $title; ?></title>
 </head>
 
 <body>
-	<h1>Welcome to my Main page view!</h1>
+	<h1><?php echo $heading; ?></h1>
 
 <?php
 
